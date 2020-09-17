bdf6e3f282a3221cc3f273c00fb28710ac2a3f96
Nico Steppat <steppat@gmail.com>
2020-09-17 08:53:27 -0300

    video1.4

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("nome", "Mundo");
+		return "hello"; 
+	}
+}
