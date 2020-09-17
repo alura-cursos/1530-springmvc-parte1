4518488486adf062a02f7eab78544ef764f21266
Nico Steppat <steppat@gmail.com>
2020-09-17 17:47:02 -0300

    video3.3

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
+		List<Pedido> pedidos = repository.findAll();
 		model.addAttribute("pedidos", pedidos);
 		return "home"; 
 	}
--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,25 +1,11 @@
 package br.com.alura.mvc.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
-
-
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	public List<Pedido> recuperaTodosOsPedidos() {
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		return query.getResultList();
-	}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
 }
