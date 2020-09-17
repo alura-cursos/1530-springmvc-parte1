60ee0f302bf6012c2794d36e224c5b630f914da3
Nico Steppat <steppat@gmail.com>
2020-09-17 20:53:46 -0300

    video4.3

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,32 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
 		return "pedido/formulario";
 	}
 	
---
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,46 @@
+package br.com.alura.mvc.mudi.dto;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+	private String nomeProduto;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setDescricao(descricao);
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		return pedido;
+	}
+	
+	
+}
