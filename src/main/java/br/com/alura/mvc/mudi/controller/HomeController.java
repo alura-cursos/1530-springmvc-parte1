ef3de07b456d355fc0c14d61489adaca4b2147b4
Nico Steppat <steppat@gmail.com>
2020-09-17 09:45:10 -0300

    video2.2

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,15 +1,28 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("nome", "Mundo");
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/81UgYuadkpL._AC_SL1500_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-64GB/dp/B07Y8YWTFL/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=Xiaomi+Redmi+Note+8&qid=1600346040&sr=8-6");
+		pedido.setDescricao("uma descrição qualquer para esse pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
+		
 		return "home"; 
 	}
 }
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,63 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
