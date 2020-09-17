820ee4f368fbc188cf1d49d686228cfff6405703
Nico Steppat <steppat@gmail.com>
2020-09-17 08:23:18 -0300

    projeto inicial

---
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/mvc/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
