a8b76887a0d6cfe52f2669b8ed76984c55eb301e
Nico Steppat <steppat@gmail.com>
2020-09-18 09:08:48 -0300

    video5.1

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +20,18 @@ public class PedidoController {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
-	@GetMapping("formulario")
-	public String formulario() {
+	@GetMapping("formulario") 
+	public String formulario(RequisicaoNovoPedido requisicao) {
 		return "pedido/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido requisicao) {
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
 		Pedido pedido = requisicao.toPedido();
 		pedidoRepository.save(pedido);
 		
--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,11 +1,18 @@
 package br.com.alura.mvc.mudi.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import br.com.alura.mvc.mudi.model.Pedido;
 
 public class RequisicaoNovoPedido {
 
+	@NotBlank
 	private String nomeProduto;
+	
+	@NotBlank
 	private String urlProduto;
+	
+	@NotBlank
 	private String urlImagem;
 	private String descricao;
 	
